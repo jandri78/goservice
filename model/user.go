8a6cd7a3c3a99f8583069dad3fa52631e39fd185
jperezsuarez_meli <jorge.perezsuarez@mercadolibre.com.co>
2025-05-06 17:15:33 -0500

    Corrections

--- model/user.go
+++ model/user.go
@@ -1,8 +1,7 @@
 package model
 
-
 type User struct {
-	ID       int    `json:"id"`
-	Name     string `json:"	Username string `json:"username"`
-	Email    string `json:"email"`
+	ID    int    `json:"id"`
+	Name  string `json:"username"`
+	Email string `json:"email"`
 }
